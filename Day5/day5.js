c20f4531795960ccf6611c5ce533f493a8267920
Elameen123 <69402639+Elameen123@users.noreply.github.com>
2022-04-09 17:55:07 +0100

    ECX 30 Days of Code Day 5

---
+++ Day5/day5.js
@@ -0,0 +1,65 @@
+staffDetails = () => {
+  // Getting Staff Name 
+  Name = document.getElementById('Name').value;
+
+   // Getting Staff Email 
+   Email = document.getElementById('Email').value;
+
+    // Getting Staff Password 
+  Password = document.getElementById('password').value;
+
+   // Getting Staff Level 
+   Level = document.getElementById('level').value;
+
+  //  Getting Staff ID 
+  // Staff ID is in the order; 
+  // OrganizationName-StaffNo.-Level-Track 
+   staffID = document.getElementById('staffID').value;
+
+    // Getting Staff Track 
+  Track = document.getElementById('track').value;
+
+// Creating an array 
+const data = [];
+data.push(Name);
+data.push(Email);
+data.push(Password);
+data.push(Level);
+data.push(staffID);
+data.push(Track);
+
+  // Separating the details by / 
+  details = data.join('/');
+  
+  staffLoginDetails = [];
+  staffLoginDetails.push(details);
+  console.log(staffLoginDetails);
+
+  const loginData = details.split('/');
+
+  // console.log(loginData);
+
+  // Login Input Labels 
+  const loginEntry = ['Name', 'Email', 'Password', 'Level', 'staffID', 'Track'];
+
+  // creating an empty object 
+  const staffData = {};
+
+  // Looping through the loginEntry array and the loginData array to pair each login Entry with its corresponding login data 
+
+  loginEntry.forEach((element, index) => {
+    staffData[element] = loginData[index];
+  }); 
+
+  console.log(staffData);
+
+  // // Getting the staff Login Details in an Array 
+  // staffArray.push(staffLoginDetails);
+
+  // console.log(staffArray);
+
+  // alert("Congratulations!!, your Login was Successful")
+
+  // console.log(staffArray);
+
+}
